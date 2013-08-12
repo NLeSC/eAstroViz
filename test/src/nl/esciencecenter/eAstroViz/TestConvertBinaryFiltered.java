d413444b01e505ffceade12ce4daf5156377eb54
Rob van Nieuwpoort <R.vanNieuwpoort@esciencecenter.nl>
2013-08-12 11:01:34 +0200

    added tiny dataset

---
+++ test/src/nl/esciencecenter/eAstroViz/TestConvertBinaryFiltered.java
@@ -0,0 +1,24 @@
+package src.nl.esciencecenter.eAstroViz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestConvertBinaryFiltered {
+
+    @Test
+    public void testWrite() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRead() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testReadFile() {
+        fail("Not yet implemented");
+    }
+
+}
