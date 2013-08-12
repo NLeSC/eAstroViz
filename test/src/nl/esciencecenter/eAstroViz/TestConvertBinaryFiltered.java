a0857cb56527a5122fe1f2517773cf1ce41d4ea6
Rob van Nieuwpoort <R.vanNieuwpoort@esciencecenter.nl>
2013-08-12 17:49:48 +0200

    Added unit test

--- test/src/nl/esciencecenter/eAstroViz/TestConvertBinaryFiltered.java
+++ test/src/nl/esciencecenter/eAstroViz/TestConvertBinaryFiltered.java
@@ -2,7 +2,14 @@ package nl.esciencecenter.eAstroViz;
 
 import static org.junit.Assert.*;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import nl.esciencecenter.eAstroViz.ConvertBinaryFiltered;
+import nl.esciencecenter.eAstroViz.dataFormats.DataProvider;
 
 import org.junit.Test;
 
@@ -14,21 +21,56 @@ public class TestConvertBinaryFiltered {
     static final int NR_POLARIZATIONS = 2;
     static final int NR_TIMES = 3 * 16 - 1;
 
+    static final int HEADER_FIELDS = 5;
+    
+    static final String INPUT_FILE_NAME = "test/fixtures/Flaggertest-01-11-11_dataset_tiny-5_stations-32_subbands-16_channels-flagged/result.filteredRaw";
+    static final String OUTPUT_FILE_NAME = "dummy.filtered";
+
     @Test
     public void testWrite() {
-        fail("Not yet implemented");
+        ConvertBinaryFiltered f = new ConvertBinaryFiltered(INPUT_FILE_NAME, OUTPUT_FILE_NAME, -1);
+        f.read();
+
+        try {
+            f.write();
+        } catch (IOException e) {
+            fail("write failed: " + e);
+        }
+
+        File file = new File(OUTPUT_FILE_NAME);
+        
+        assertTrue(file.exists());
+
+        assertEquals(HEADER_FIELDS * DataProvider.SIZE_OF_FLOAT + NR_STATIONS * NR_TIMES * NR_SUBBANDS * NR_CHANNELS * NR_POLARIZATIONS * DataProvider.SIZE_OF_FLOAT, file.length());
+        
+        FileInputStream fin = null;
+        try {
+            fin = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            fail("open failed: " + e);
+        }
+        
+        DataInputStream din = new DataInputStream(fin);
+        
+        try {
+            assertEquals(NR_STATIONS, din.readInt());
+            assertEquals(NR_TIMES, din.readInt());
+            assertEquals(NR_SUBBANDS, din.readInt());
+            assertEquals(NR_CHANNELS, din.readInt());
+            assertEquals(NR_POLARIZATIONS, din.readInt());
+        } catch (IOException e) {
+            fail("read failed: " + e);
+        }
     }
 
     @Test
     public void testRead() {
-        String inputFileName = "test/fixtures/Flaggertest-01-11-11_dataset_tiny-5_stations-32_subbands-16_channels-flagged/result.filteredRaw";
-        String outputFileName = "dummy";
 
-        ConvertBinaryFiltered f = new ConvertBinaryFiltered(inputFileName, outputFileName, -1);
+        ConvertBinaryFiltered f = new ConvertBinaryFiltered(INPUT_FILE_NAME, OUTPUT_FILE_NAME, -1);
         f.read();
 
-        assertEquals(inputFileName, f.getFileName());
-        assertEquals(outputFileName, f.getOutputFileName());
+        assertEquals(INPUT_FILE_NAME, f.getFileName());
+        assertEquals(OUTPUT_FILE_NAME, f.getOutputFileName());
         assertEquals(-1, f.getMaxNrTimes());
         assertEquals(NR_CHANNELS, f.getNrChannels());
         assertEquals(NR_POLARIZATIONS, f.getNrPolarizations());
@@ -69,24 +111,4 @@ public class TestConvertBinaryFiltered {
         System.err.println("nrSamples = " + nrSamples + ", flagged = " + flagged + ", diffs = " + diffs);
         assertEquals(flagged + diffs, nrSamples);
     }
-
-    /*
-             for (int station = 0; station < data.length; station++) {
-                for (int time = 0; time < data[0].length; time++) {
-                    for (int sb = 0; sb < data[0][0].length; sb++) {
-                        for (int ch = 0; ch < data[0][0][0].length; ch++) {
-                            for (int pol = 0; pol < data[0][0][0][0].length; pol++) {
-                                
-                            }
-                        }
-                    }
-                }
-            }
-
-     */
-    @Test
-    public void testReadFile() {
-        fail("Not yet implemented");
-    }
-
 }
